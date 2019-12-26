de46055f7567015636041d3d3ac9a6ce8e9d3109
Brooke Cheng <chenghe123@gmail.com>
2019-12-26 14:12:41 -0500

    Coming Soon Page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Homepage from './Components/Homepage/Homepage'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Container">
+        <Homepage></Homepage>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Homepage/Homepage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Homepage.css';
+
+class Homepage extends React.Component {
+    render() {
+      return <div className="HomePage">
+        <p className="TwoHandsLogo"/>
+        <div className="Title">
+            Two Hands Collective
+        </div>
+        <div className="Subtitle">
+            Coming Soon
+        </div>
+        </div>
+    }
+}
+
+export default Homepage;
