c824daa3b73573f36d7782f74f4ce041f7932f38
Brooke Cheng <chenghe123@gmail.com>
2019-12-26 15:48:28 -0500

    Added founded message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Components/Homepage/Homepage'
+import Homepage from './Components/Homepage/Homepage';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <div className="App-Container">
         <Homepage></Homepage>
+        <Footer></Footer>
       </div>
     </div>
   );
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    render() {
+      return <div className="Footer">
+          Founded by Brooke Cheng and <a href="http://liacoleman.com"> Lia Coleman</a>
+        </div>
+    }
+}
+
+export default Footer;
