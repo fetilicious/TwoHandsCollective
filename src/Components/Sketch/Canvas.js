87834ee513e401f4964db118996112317c076e94
Lia Coleman <liailiad@gmail.com>
2020-01-03 15:22:19 -0800

    add interactive circles

--- src/Components/Sketch/Canvas.js
+++ src/Components/Sketch/Canvas.js
@@ -4,28 +4,42 @@ import './Canvas.css';
 
 class Canvas extends React.Component {
     render() {
-        let lines = [];
+        let pts = [];
         return (
         <div className="SketchContainer">
             <Sketch
-            setup={(p5, canvasParentRef) => {
+            setup={
+              (p5, canvasParentRef) => {
                 p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
             }}
             draw={
                 p5 => {
-                    if(p5.pmouseX !== p5.mouseX && p5.pmouseY !== p5.mouseY){
-                        var curPt = p5.createVector(p5.mouseX, p5.mouseY);
-                        lines.push(curPt);
+                      var startRad = 50;
+                      var curPt = p5.createVector(p5.mouseX, p5.mouseY, startRad);
+                      pts.push(curPt);
+                      if(pts.length > 50){
+                        pts.shift(); //rm head
                       }
-                     
-                      console.log(lines.length);
-                      if(lines.length > 50){
-                        lines.shift(); //rm head
+
+                      p5.background(33, 33, 33);
+
+                      for (var i = 0; i < pts.length; i++){
+                        p5.stroke(50);
+
+                        p5.circle(
+                          pts[i].x,
+                          pts[i].y,
+                          pts[i].z--,
+                        );
+
+                        // p5.line(
+                        //   p5.windowWidth / 2., p5.windowHeight / 2., 
+                        //   pts[i].x, pts[i].y
+                        // );
                       }
-                     
-                      for (var i = 0; i < lines.length; i++){
-                        p5.stroke(256);
-                        p5.line(0, 0, lines[i].x, lines[i].y);
+
+                      if(p5.mouseIsPressed){
+                        pts = [];
                       }
                 }
             }
